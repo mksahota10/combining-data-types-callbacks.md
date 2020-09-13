80b3063f49540f873c7e489cfdeb38af9d9a53e6
mksahota10 <mksahota10@gmail.com>
2020-09-12 18:50:31 -0700

    Combine objects, arrays, and functions more than one level deep

--- combining_datatypes.js
+++ combining_datatypes.js
@@ -33,3 +33,63 @@ console.log(receipt.price);
 
 let apartmentBuilding = ["Preet", { tenant: "Leah" }, "Sedef", "Vin"];
 console.log(apartmentBuilding[1].tenant);
+
+//Create a function knit that returns an object that has the following kinds of properties item: scarf, size : 6ft etc. Log a value of that object (hint: call the function and then call a property on the return value).
+const knit = {
+  newObject: {
+    item: 'scarf',
+    size: 3
+  }
+}
+console.log(knit.newObject.item);
+
+
+
+
+//I was stuck on this for some reason, Edward helped me.
+
+//Create a function crayonSelector that returns an object that has an array (you can reuse your crayonBox object). Log one of the elements of that array.
+
+var crayonSelector = {
+  crayonBox: { 
+    color:['blue', 'green', 'purple']
+  }
+}
+console.log(crayonSelector.crayonBox.color[2]);
+
+
+//Create a function powerButton that returns a function called options - options should console.log a simple message like select a song. Call that inner function
+
+// const powerButton () {
+//   options: {
+//     song: "secrets",
+//   }
+// }
+// console.log('select a song');
+
+
+// function myFunction(p1, p2) {
+//   return p1 * p2; // The function returns the product of p1 and p2
+// }
+
+
+
+
+
+const powerButton = 'secrets'
+
+const options = () => {
+options: ['secrets','try me', 'bop']
+  console.log(seclect a song);
+ 
+}
+options()
+
+
+// const item = 'spicy meatball';
+
+// const exampleFunction = () => {
+//     console.log(item + " within function");
+// }
+
+// exampleFunction();
